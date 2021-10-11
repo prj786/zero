e658674009bc09dfe50c6e2fa545ba66cc2c8750
giorgi.macharashvili <giorgi.macharashvili@ventiuno.io>
2021-10-11 20:03:49 +0400

    final code base

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,22 +3,10 @@ const path = require('path');
 
 const config = require('./webpack.config');
 
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-
 module.exports = merge(config, {
   mode: 'development',
 
   devtool: 'inline-source-map',
-
-  plugins: [
-    new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development,
-      // ./public directory is being served
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['dev'] }
-    })
-  ],
 
   devServer: {
     writeToDisk: true,
